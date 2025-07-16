8d7bd5c005feeb4a5fbbb3b01af44a8e2e0bb57f
Ruslan Popesku <ruslanpopesku95@gmail.com>
2025-07-16 12:32:42 +0300

    [back]

--- Offline AI&ML Playground/DownloadTab/ModelDownloadManager.swift
+++ Offline AI&ML Playground/DownloadTab/ModelDownloadManager.swift
@@ -107,7 +107,7 @@ class ModelDownloadManager: NSObject, ObservableObject {
     override init() {
         // Setup directories
         self.documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        self.modelsDirectory = documentsDirectory.appendingPathComponent("MLXModels", isDirectory: true)
+        self.modelsDirectory = documentsDirectory.appendingPathComponent("Models", isDirectory: true)
         
         super.init()
         
